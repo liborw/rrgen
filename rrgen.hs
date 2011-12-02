4b0e04cf9635b85488621b14b0e9d97f27c79ef8
Libor Wagner <liborwagner@gmail.com>
2011-12-02 17:13:13 +0100

    General polygon

--- rrgen.hs
+++ rrgen.hs
@@ -1,41 +1,41 @@
-type Point     = (Float,Float)
-type Color     = (Int,Int,Int)
-type Polygon   = [Point]
+type Point      = (Float,Float)
+type Color      = (Int,Int,Int)
+type Polygon    = [Point]
 
-writePoint :: Point -> String 
-writePoint (x,y) = (show x)++","++(show y)++" "
+writePoint :: Point -> String
+writePoint (x,y) = show x ++ "," ++ show y ++ " "
 
-writePolygon :: (Color,Polygon) -> String 
-writePolygon ((r,g,b),p) = "<polygon points=\""++(concatMap writePoint p)++"\" style=\"fill:#cccccc;stroke:rgb("++(show r)++","++(show g)++","++(show b)++");stroke-width:2\"/>"
+writePolygon :: Polygon -> String
+writePolygon p = "<polygon points=\"" ++ concatMap writePoint p
+                ++ "\" style=\"fill:#ffffff;stroke:#000000;stroke-width:2\"/>"
 
-writePolygons :: [(Color,Polygon)] -> String 
-writePolygons p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"++(concatMap writePolygon p)++"</svg>"
+writePolygons :: [Polygon] -> String
+writePolygons p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"
+                    ++ concatMap writePolygon p
+                    ++ "</svg>"
 
-colorize :: Color -> [Polygon] -> [(Color,Polygon)] 
-colorize = zip.repeat
+polygon :: Int -> Float -> Polygon
+polygon n r = [(x i, y i)| i <- [0..(n-1)]]
+        where a = (2*pi) / fromIntegral n
+              x i = r * cos (a * fromIntegral i)
+              y i = r * sin (a * fromIntegral i)
 
-rainbow@[red,green,blue,yellow,purple,teal] = map colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
+hexagon :: Float -> Polygon
+hexagon = polygon 6
 
-t0 = writeFile "tut0.svg" $ writePolygons (blue [[(100,100),(200,100),(200,200),(100,200)],[(200,200),(300,200),(300,300),(200,300)]])
+translateTo :: Point -> Polygon -> Polygon
+translateTo (x,y) poly = map f poly where f (a,b) = (a + x, b + y)
 
-hexagon c r = translateTo c basicHexagon where
-  basicHexagon =  top ++ (negate r, 0):bottom 
-  top          =  [(r,0),(r * cos 1,(r * sin 1)),(negate (r * cos 1), r * (sin 1))]
-  bottom       =  map (\(x,y)->(x,negate y)) (reverse top)
+-- hexField :: Float -> Int -> Int -> [Polygon]
+-- hexField r n m = let
+--     mkHex n = hexagon (1.5*(fromIntegral n)*(r*2),(r*2)) r
+--     row n = map mkHex [1..n]
+--     aRow = row n
+--  in concat [map (offset (r*(fromIntegral x))) aRow |x<-[1..m]]
 
-translateTo (x,y) poly = map f poly where f (a,b)= ((a+x),(b+y))
-
-t1 = writeFile "t1.svg" $ writePolygons (blue [hexagon (100,100) 50] )
-
-hexField r n m = let 
-     mkHex n = hexagon (1.5*n*(r*2),(r*2)) r
-     row n = map mkHex [1..n]
-     aRow = row n
-  in concat [map (offset (r*x)) aRow |x<-[1..m]]
-
+offset :: Float -> Polygon -> Polygon
 offset r polys = map (oh r) polys where
   oh r pt@(x,y) = (x+(1.5*r),y+(r*sin 1))
 
-t2 = writeFile "t2.svg" $ writePolygons (blue $ hexField 50 4 5 )
 
-main = t2
+main = putStrLn $ writePolygons [translateTo (100, 100) $ polygon 6 50]
