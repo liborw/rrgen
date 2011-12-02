0a921e9ee9059228576dcd15b1a281fddc520df2
Libor Wagner <liborwagner@gmail.com>
2011-12-02 20:11:59 +0100

    Hexagon grid

--- rrgen.hs
+++ rrgen.hs
@@ -15,13 +15,33 @@ writePolygons p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"
                     ++ "</svg>"
 
 polygon :: Int -> Float -> Polygon
-polygon n r = [(x i, y i)| i <- [0..(n-1)]]
+polygon n r = [(x i, y i) | i <- [0..(n-1)]]
         where a = (2*pi) / fromIntegral n
               x i = r * cos (a * fromIntegral i)
               y i = r * sin (a * fromIntegral i)
 
+polygon' :: Int -> Float -> Point -> Polygon
+polygon' n r p = translateTo p (polygon n r)
+
 hexagon :: Float -> Polygon
 hexagon = polygon 6
+
+hexagon' :: Float -> Point -> Polygon
+hexagon' r p = translateTo p (hexagon r)
+
+
+pi2 = 2 * pi
+
+hexGridPoints :: Float -> Int -> Int -> [Point]
+hexGridPoints r n m = let
+    yOffset = 2 * r * sin (pi2/6)
+    xOffset = r + r * sin (pi2/12)
+    y i j
+     | odd j        = (yOffset * fromIntegral i) + (yOffset/2)
+     | otherwise    = (yOffset * fromIntegral i) + yOffset
+    x i j = (xOffset * fromIntegral j) + r
+    in [ (x i j, y i j) | i <- [0..(n-1)], j <- [0..(m-1)]]
+
 
 translateTo :: Point -> Polygon -> Polygon
 translateTo (x,y) poly = map f poly where f (a,b) = (a + x, b + y)
@@ -31,4 +51,4 @@ offset r polys = map (oh r) polys where
   oh r pt@(x,y) = (x+(1.5*r),y+(r*sin 1))
 
 
-main = putStrLn $ writePolygons [translateTo (100, 100) $ polygon 6 50]
+main = putStrLn $ writePolygons (map (hexagon' 10) (hexGridPoints 10 38 66))
